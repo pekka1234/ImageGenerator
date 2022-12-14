1e6009d3271f183bbc919c215feae255ff1b6f33
pekka1234 <62663286+pekka1234@users.noreply.github.com>
2022-12-14 18:54:24 +0200

    bug fixes

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 
-// functuionazing copied code (ranord atleast)
+// functuionazing copied code (ranord atleast) TEXT:POPBACKAUSmuuallaki kuin eachepissä JAORDER PATTERNS ELI ESIM. paljon musttaa ja valkoista peräkkäin
 
 
 // text is the string that will be written to the file, w is image width, h is image height
@@ -43,23 +43,61 @@ int random(int min, int max){
 
 
 // function that is used when typing order and when order is randomly generated with lenght, function basically creates the image content when using orders
-void orde(vector<string> sequ){
+void orde(vector<string> sequ, bool swwi){
     int c = 0;
-    for(int i = 0; i < h; i++){
-        for(int x = 0; x < w; x++){
-            text += sequ[c];
-            if(c != (sequ.size() - 1)){
-	            c += 1;
+    if(swwi){
+        for(int i = 0; i < h; i++){
+            for(int x = 0; x < w; x++){
+                text += sequ[c];
+                if(c != (sequ.size() - 1)){
+                    c += 1;
+                }else{
+                    c = 0;
+                }
+                if(x != (w - 1)){
+                    text += ' ';
+                }else if(i != (h - 1)){
+                    text += '\n';
+                }
+            }
+        }    
+    }else{
+        vector<string> finale;
+        string temp;
+        string tempi;
+        string cur;
+        int sequz = sequ.size();
+
+        for(int u = 0; u < sequz; u++){
+            temp = {};
+            cur = sequ[u];
+            for(int y = 0; y < 9; y++){
+                if((y + 1) % 3 != 0){
+                    tempi += cur[y];
+                }else{
+                    tempi += cur[y];
+                    temp += (to_string((stoi(tempi))) + ' ');
+                    tempi = "";
+                }
+            }
+            temp.pop_back();
+            finale.push_back((temp));
+        }
+
+        int lenf = finale.size();
+        for(int i = 0; i < (w * h); i++){
+            if(i != (w * h - 1)){
+                text += (finale[c] + '\n');
+            }else{
+                text += finale[c];
+            }
+            if(c < (lenf - 1)){
+                c += 1;
             }else{
                 c = 0;
             }
-            if(x != (w - 1)){
-                text += ' ';
-            }else if(i != (h - 1)){
-                text += '\n';
-            }
-	    }
-    }    
+        }
+    }
 }
 
 
@@ -93,114 +131,76 @@ vector<string> split(string str, bool smalbig){
 }
 
 
-void putseq(){
+void putseq(bool swi){
     // asking for order and running function with it
     string seq;
     vector<string> relseq;
-    cout << "Enter order of blacks(1) and whites(0) the image will be filled(for exmp. 1000110): ";
-    cin >> seq;
-    relseq = split(seq, false);
-    if(lines(relseq.size())){
-        putseq();
-    }else{
-        orde(relseq);
-    }	
-}
-
-
-
-
-// manual input order for color images
-void corde(vector<string> cequ){
-    vector<string> finale;
-    string temp;
-    string tempi;
-    int ier = 0;
-    string cur;
-    int cequz = cequ.size();
-    //for (string g: cequ)
-    //    cout << g << ' ';
-    //cout << cequz << 'g';    
-    for(int u = 0; u < cequz; u++){
-        temp = {};
-        cur = cequ[u];
-        for(int y = 0; y < 9; y++){
-            if((y + 1) % 3 != 0){
-                tempi += cur[y];
-            }else{
-                tempi += cur[y];
-                temp += (to_string((stoi(tempi))) + ' ');
-                tempi = "";
-            }
-        }
-        temp.pop_back();
-        finale.push_back((temp));
-    }
-    /*
-    for(string h: finale)
-        cout << h << ' ';
-    */
-    int lenf = finale.size();
-    for(int i = 0; i < (w * h); i++){
-        if(i != (w * h - 1)){
-            text += (finale[ier] + '\n');
+    if(swi){
+        cout << "Enter order of blacks(1) and whites(0) the image will be filled(for exmp. 1000110): ";
+        cin >> seq;
+        relseq = split(seq, false);
+        if(lines(relseq.size())){
+            putseq(true);
         }else{
-            text += finale[ier];
-        }
-        if(ier < (lenf - 1)){
-            ier += 1;
-        }else{
-            ier = 0;
-        }
-    }
-    
-}
-
-void cutseq(){
-    string ceq;
-    cout << "Enter order of rgb values (max 255) seperated by ',' (for exmp. 012233002,111123010,255001000): ";
-    cin >> ceq;
-    vector<string> celseq = split(ceq, true);
-    if(lines(celseq.size())){
-        cutseq();
+            orde(relseq, true);
+        } 
     }else{
-        corde(celseq);
+        cout << "Enter order of rgb values (max 255) seperated by ',' (for exmp. 012233002,111123010,255001000): ";
+        cin >> seq;
+        relseq = split(seq, true);
+        if(lines(relseq.size())){
+            putseq(false);
+        }else{
+            orde(relseq, false);
+        }
     }
 }
 
-void eachsep(){
+
+void eachsep(bool swt){
     // randomizing for each pixel seperately
-    for(int d = 0; d < h; d++){
-        for(int f = 0; f < w; f++){
-            text += to_string(random(0,1));
-            if(f != (w - 1)){
-                text += ' ';
-            }else if(d == (h - 1)){
-                text += '\n';
+    if(swt){
+        for(int d = 0; d < h; d++){
+            for(int f = 0; f < w; f++){
+                text += to_string(random(0,1));
+                if(f != (w - 1)){
+                    text += ' ';
+                }else{
+                    text += '\n';
+                }
             }
         }
+    }else{
+        for(int i = 0; i < (w * h); i++){
+            text += (to_string(random(0,255)) + ' ' + to_string(random(0,255)) + ' ' + to_string(random(0,255)) + '\n');
+        }
     }
+    text.pop_back();
 }
 
+// tells order what was used generating the image (when random order is thing is used)
+void usedorder(vector<string> ord, char del){
+    cout << "Used order: ";
+    for(string i: ord)
+        cout << i << del;
+    cout << '\n';
+}
 
 void rcord(bool sw){
-    // creating random order (colors) and running corde function with it
+    // creating random order and running orde function with it
     string len;
     cout << "Enter order's length: ";
     cin >> len;  
     int rlen = stoi(len);
     if(lines(rlen)){
-        rcord(false);        
+        rcord(sw);        
     }else if(sw){
         vector<string> rels;
         for(int y = 0; y < rlen; y++){
             rels.push_back(to_string(random(0,1)));
         }
-        cout << "Used order: ";
-        for(string i: rels)
-            cout << i;
-        cout << '\n';
-        orde(rels);
+        usedorder(rels, '\0');
+        orde(rels, true);
     }else{
         vector<string> cels;
         for(int i = 0; i < rlen; i++){
@@ -216,16 +216,9 @@ void rcord(bool sw){
             }
             cels.push_back(rs);
         }
-        corde(cels);
+        usedorder(cels, ' ');
+        orde(cels, false);
     }
-}
-
-
-void cachep(){
-    for(int i = 0; i < (w * h); i++){
-        text += (to_string(random(0,255)) + ' ' + to_string(random(0,255)) + ' ' + to_string(random(0,255)) + '\n');
-    }
-    text.pop_back();
 }
 
 // asks question of the image and runs functions
@@ -251,7 +244,7 @@ int main(){
         cin >> sor;
 
         if(sor == "0"){
-            putseq();
+            putseq(true);
         }else{
             char roor;
             cout << "Random for each pixel(0) or random order(1): ";
@@ -259,7 +252,7 @@ int main(){
             if(roor == '1'){
                 rcord(true);
             }else{
-                eachsep();
+                eachsep(true);
             }
         }
     }else{
@@ -268,7 +261,7 @@ int main(){
         cout << "Colors in orded order(0) or randomly generated(1): ";
         cin >> csor;
         if(csor == '0'){
-            cutseq();
+            putseq(false);
         }else{
             char anw;
             cout << "Random for each pixel(0) or random order(1): ";
@@ -276,7 +269,7 @@ int main(){
             if(anw == '1'){
                 rcord(false);
             }else{
-                cachep();
+                eachsep(false);
             }
         }
     }
